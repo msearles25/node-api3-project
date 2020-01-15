6fb775b4340ecfd6f2d89acc9baad1fbe5573faa
Michael <skaman4459@gmail.com>
2020-01-15 16:50:36 -0500

    progress made

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 const express = require('express');
 
+const usersRouter = require('./users/userRouter');
+const postsRouter = require('./posts/postRouter');
+
 const server = express();
+
+server.use(express.json());
+
+server.use('/api/users', usersRouter);
+server.use('/api/posts', postsRouter);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`);
